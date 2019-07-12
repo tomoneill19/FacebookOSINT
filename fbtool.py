ae206920a6b07af094408daeb2baa1bf43fc7249
Tom O'Neill <tomoneill19@gmail.com>
2019-07-12 11:40:56 +0100

    added menu + welcome message

--- fbtool.py
+++ fbtool.py
@@ -1,11 +1,51 @@
 #!/usr/bin/env python
 import webbrowser
+import signal
+import sys
+
+def help():
+    print("Commands: placeholder, placeholder2" )
+
 
 def buildURL():
     return ("http://www.google.com/search?q=test")
 
+
 def gotoURL(url):
     webbrowser.open_new_tab(url)
 
-if __name__ == "__main__":
-    gotoURL(buildURL())
+
+def parse_cmd(cmd):
+    if cmd == "help":
+        help()
+
+def menu():
+    print("Menu>", end = "")
+    parse_cmd(input().lower())
+
+
+def exit(signal, frame):
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, exit)
+
+
+
+
+banner = '''
+  ______ ____   ____   _____ _____ _   _ _______
+  |  ____|  _ \ / __ \ / ____|_   _| \ | |__   __|
+  | |__  | |_) | |  | | (___   | | |  \| |  | |
+  |  __| |  _ <| |  | |\___ \  | | | . ` |  | |
+  | |    | |_) | |__| |____) |_| |_| |\  |  | |
+  |_|    |____/ \____/|_____/|_____|_| \_|  |_|
+
+'''
+print(banner)
+print("Welcome to the Facebook OSINT tool by Tom (@tomoneill19)")
+print("Type \"help\" for a list of commands")
+
+while True:
+    menu()
+
