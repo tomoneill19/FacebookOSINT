21b6e0a671d2f8e91af9865590619a9c13817999
Tom O'Neill <tomoneill19@gmail.com>
2019-07-12 10:20:23 +0100

    made methods for getting urls

--- fbtool.py
+++ fbtool.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
 import webbrowser
 
-webbrowser.open_new_tab("http://www.google.com/search?q=test")
+def buildURL():
+    return ("http://www.google.com/search?q=test")
+
+def gotoURL(url):
+    webbrowser.open_new_tab(url)
+
+if __name__ == "__main__":
+    gotoURL(buildURL)
